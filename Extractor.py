98f758bb5f6b55fc7a35c00a80122c7998b5ec57
wentian <983139929@qq.com>
2021-08-13 19:45:43 +0800

    codeBlockList complete

--- Extractor.py
+++ Extractor.py
@@ -1,19 +1,36 @@
 import time
 
 
-
+#代码块node
 class Node (object):
     def __init__(self, index,Range,layer,data):
         """链表中节点结构，只有data和next，多跳转情况下需调整
             :param data:
             :return:
         """
-        self.index = index
-        self.Range = Range
+        self.index = index#每个node的唯一编号。编号顺序按深度优先
+        self.Range = Range#该代码块在原字符串中坐标范围
         self.data = data
-        self.layer =layer
+        self.label = []#用于存储该代码块的控制语句，由于嵌套可能存在，故将label定义为数组.其值为[(condition，index),...]
+        self.layer =layer#相对于程序入口，该代码块的嵌套层数
         self.next = None
 
+    def show(self):
+        print("index of the code blocks is \n\t{}".format(self.index))
+        print("Range of source code is \n\t{}".format(self.Range))
+        print("data of the code block is \n\t{}".format(self.data))
+        print("label of the code blocks is \n\t{}".format(self.label))
+        print("layer of the code blocks is \n\t{}".format(self.layer))
+
+#用于生成node的label的数据结构
+class LabelNode(object):
+    def __init__(self,layer):
+        self.value = []#用于存储该代码块的控制语句，由于嵌套可能存在，故将value定义为数组.其值为[(condition，index),...]
+        #由于是深度优先，所以当有深层嵌套时，next必有值
+        self.next = None
+        self.layer = layer#label是第几层嵌套的label
+
+#动态单链表，用于labelnode生成为每个代码块node生成label
 class Linklist(object):
 
     def __init__(self):
@@ -29,7 +46,7 @@ class Linklist(object):
             pre = pre.next
         return length
 
-    def append(self,data):
+    def append(self,labelnode):
         """
             追加节点，需要新建节点（多跳转情况下需调整）\n
             1.head 为none :head-->node\n
@@ -38,7 +55,7 @@ class Linklist(object):
             :return:
         """
 
-        new_node = Node(data)
+        new_node = labelnode
 
         #若为空表，则新节点为表头
         if self.head == None:
@@ -68,8 +85,6 @@ class Linklist(object):
             index -= 1
         return pre
 
-
-
     def set(self, index, data):
         """根据index更新节点data
         """
@@ -78,7 +93,7 @@ class Linklist(object):
             node.data = data
         return node
 
-
+#读取数据转为字符串
 def readFileToStr(filename)->str:
     try:
         f = open(filename, "r")
@@ -111,6 +126,7 @@ def readFileToStr(filename)->str:
 
     return resultStr
 
+#在字符串中返回子串所在位置的list
 def find(string,subString)->list:
     """
     返回子串所在位置的list
@@ -132,7 +148,7 @@ def find(string,subString)->list:
         # move to next possible start position
         start = index + 1
     return result
-
+#用于括号匹配的数据结构
 class brace (object):
     def __init__(self, index,layer):
         """括号匹配，并且分出代码层数（只需要看左括号）/n
@@ -148,14 +164,37 @@ class brace (object):
         self.next = next
         return
 
-def codeBlockExtractor(content:str):
+#用于管理一份代码的所有codeBlock（即node）
+class codeBlockList(object):
+    def __init__(self):
+        self.nodelist = []
+        self.length = 0
+    def append(self,node):
+        self.nodelist.append(node)
+    def getLength(self):
+        self.length = len(self.nodelist)
+        return self.length
+    def show(self):
+        for j in range(len(self.nodelist)):
+            print("-----------codeBlock%d---------------" % j)
+            self.nodelist[j].show()
+            print("---------------------------")
+    #各代码块顺序按在文件中的顺序排列
+    def sortByBraceOrder(self):
+        self.nodelist.sort(key=self.takeNodeBrace)
+    def takeNodeBrace(self,elem):
+        return elem.Range[0]
 
+#从代码中提取codeBlock
+def codeBlockExtractor(content:str):
+    print("-----------codeBlockExtractor---------------")
     cb = []
     #array数组用于记录所有brace，并判断其所处层数
-    array = [];atop = -1;
+    # array = [];atop = -1;
     #stack栈用于记录括号匹配信息，在左括号进栈之后,栈的深度stop的值等于左括号所处的层数layer
     stack = [];stop = -1;
 
+    #利用brace括号匹配生成代码块，形成初步的node list
     #layer = 0,1,2,3....
     for i in range(len(content)):
         c = content[i]
@@ -163,8 +202,8 @@ def codeBlockExtractor(content:str):
             #初始化一定为左括号，进栈
             layer = stop+1;
             temp = brace(i,layer)
-            array.append(temp);
-            atop += 1;
+            # array.append(temp);
+            # atop += 1;
             stack.append(temp);
             stop += 1;
             # print("--------1---------------")
@@ -177,8 +216,8 @@ def codeBlockExtractor(content:str):
                 #若当前为左括号，进栈
                 layer = stop + 1;
                 temp = brace(i, layer)
-                array.append(temp);
-                atop += 1;
+                # array.append(temp);
+                # atop += 1;
 
                 if  len(stack)-1 > stop and stack[stop+1] == None :
                     stack[stop+1] = temp
@@ -203,8 +242,8 @@ def codeBlockExtractor(content:str):
                  # print("stack is {}\n".format(stack[stop]))
                  cb.append(stack[stop])
 
-                 array.append(temp);
-                 atop += 1;
+                 # array.append(temp);
+                 # atop += 1;
                  stack[stop]=None;
                  stop -= 1;
 
@@ -246,30 +285,50 @@ def codeBlockExtractor(content:str):
         #         atop += 1;
         #         stack[stop]=None;
         #         stop -= 1;
-    nodeList = []
+    nodeList = codeBlockList()
     for j in range(len(cb)):
-        print("-----------%d---------------" % j)
-        print(cb[j].index)
-        print(cb[j].next.index)
-        print(cb[j].layer)
-        data = content[cb[j].index:cb[j].next.index+1]
-        print(data)
+
+        data = content[cb[j].index:cb[j].next.index + 1]
+
+        # print("-----------%d---------------" % j)
+        # print(cb[j].index)
+        # print(cb[j].next.index)
+        # print(cb[j].layer)
+        # print(data)
+
         Range = (cb[j].index,cb[j].next.index)
-        temp = Node(len(cb)-j,Range,cb[j].layer,data)
+        #temp = Node(len(cb)-j,Range,cb[j].layer,data)
+        temp = Node(j, Range, cb[j].layer, data)
 
         nodeList.append(temp)
-    nodeList.reverse();
+    #nodeList是所有代码块node的list
+
+    #nodeList.reverse();#反序前按深度优先排列,反序后node的index顺序中第一个是程序入口处的代码块
+    #nodeList中index按深度优先遍历
+    # for j in range(len(nodeList)):
+    #     print("-----------codeBlock%d---------------" % j)
+    #     nodeList[j].show()
+    #     print("---------------------------")
+
+    nodeList.show()
+    print("-----------End of codeBlockExtractor---------------")
     return nodeList
 
-
+#从代码和codeBlockList中提取出CFG
 def cfgExtractor(content:str):
-
-    cb = codeBlockExtractor(content)
-    p_if = find(content,"if")
-    p_else = find(content,"else")
-
-    print(p_if)
-    print(p_else)
+    print("-----------cfgExtractor---------------")
+    cbList = codeBlockExtractor(content)
+    # p_if = find(content,"if")
+    # p_else = find(content,"else")
+    # print(p_if)
+    # print(p_else)
+    cbList.sortByBraceOrder()
+    # cbList.sort(key=takeNodeBrace)
+    # for j in range(len(cbList)):
+    #     print("-----------codeBlock%d---------------" % j)
+    #     cbList[j].show()
+    #     print("---------------------------")
+    cbList.show()
     return
 
 def main():
