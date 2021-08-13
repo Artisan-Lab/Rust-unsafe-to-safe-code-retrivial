20a49a9e92b72a31b853703acb039127457fa751
wentian <983139929@qq.com>
2021-08-14 00:08:14 +0800

    check fuction to do

--- Extractor.py
+++ Extractor.py
@@ -1,7 +1,7 @@
 import time
 
 
-#代码块node
+#代码块node,node of code block
 class Node (object):
     def __init__(self, index,Range,layer,data):
         """链表中节点结构，只有data和next，多跳转情况下需调整
@@ -11,27 +11,107 @@ class Node (object):
         self.index = index#每个node的唯一编号。编号顺序按深度优先
         self.Range = Range#该代码块在原字符串中坐标范围
         self.data = data
-        self.label = []#用于存储该代码块的控制语句，由于嵌套可能存在，故将label定义为数组.其值为[(condition，index),...]
+        self.title = None#每个代码块的左括号前，上一条语句之后的语句
+        self.label = []#用于存储该代码块的控制语句，由于嵌套可能存在，故将label定义为数组.其值为[(index,condition),...]
         self.layer =layer#相对于程序入口，该代码块的嵌套层数
+        self.findScope = None  # 代码块中寻找标签的源代码中的范围
         self.next = None
 
     def show(self):
-        print("index of the code blocks is \n\t{}".format(self.index))
+        print("layer of the code blocks is \n\t{}".format(self.layer))
+        print("index of the code block is \n\t{}".format(self.index))
+        print("findScope of the code block is {}".format(self.findScope))
+        print("title of the code block is \n\t{}".format(self.title))
         print("Range of source code is \n\t{}".format(self.Range))
         print("data of the code block is \n\t{}".format(self.data))
         print("label of the code blocks is \n\t{}".format(self.label))
-        print("layer of the code blocks is \n\t{}".format(self.layer))
+#用于管理一份代码的所有codeBlock（即node）
+class codeBlockList(object):
+    def __init__(self):
+        self.nodelist = []
+        self.length = 0
+
+    def append(self,node):
+        self.nodelist.append(node)
+
+    def getLength(self):
+        self.length = len(self.nodelist)
+        return self.length
+
+    def calFindScope(self, content):
+        #确定各个节点的label计算范围，并赋值title
+        #range index of content
+        start = 0
+
+        end = len(content)-1
+        #遍历nodelist，确定各个节点寻找关键字的范围。被遍历的nodelist应是被sortByBraceOrder排序过的
+        for i in range(len(self.nodelist)):
+            #关键字范围对应该代码块左括号之前的第一条语句
+            print("--------findScope of node %d------------" % i)
+            if i == 0:
+                r = self.nodelist[i].Range
+                self.nodelist[i].findScope = (start,r[0])#寻找范围不包括本代码块的括号
+                # print("findScope is \n\t{}".format(self.nodelist[i].findScope))
+                # print("the string of findScope in sorce code is \n\t{}".format(content[start:r[0]]))
+                self.nodelist[i].title = content[start:r[0]]
+                start = r[0]#下次搜索的起始寻找范围不包括本代码块的括号
+            else:
+                r = self.nodelist[i].Range
+                tempscope = (start, r[0])#上个代码块开始的地方，到这个代码块开始的地方
+                # print("tempscope is \n\t{}".format(tempscope))
+                # print("tempscope is \n\t{}".format(content[start:r[0]]))
+                for j in range(r[0]-1,start,-1):#range是左闭右开，所以不包含本代码块的括号
+                    #在这个范围内寻找;{}进一步缩小范围，且不包含这些符号
+                    if content[j] == ';' or content[j] == '{' or content[j] == '}':
+                        start = j+1
+                        break
+                # self.nodelist[i].findScope = (start,r[0])
+                # print("findScope is \n\t{}".format(self.nodelist[i].findScope))
+                # print("the string of findScope in sorce code is \n\t{}".format(content[start:r[0]]))
+                self.nodelist[i].title = content[start:r[0]]
+                start = r[0]
+
+            pass
+        return
+
+    def changeLabel(self,tempnode):
+        self.nodelist[tempnode.index].label = tempnode.label
+        return
+
+    def show(self):
+        for j in range(len(self.nodelist)):
+            print("-----------codeBlock%d---------------" % j)
+            self.nodelist[j].show()
+            print("---------------------------")
+    #各代码块顺序按在文件中的顺序排列,并改变index
+    def sortByBraceOrder(self):
+        self.nodelist.sort(key=self.takeNodeBrace)
+        for j in range(len(self.nodelist)):
+            # print("-----------codeBlock%d---------------" % j)
+            self.nodelist[j].index = j
+            # print("---------------------------")
+
+    def takeNodeBrace(self,elem):
+        return elem.Range[0]
 
 #用于生成node的label的数据结构
 class LabelNode(object):
     def __init__(self,layer):
         self.value = []#用于存储该代码块的控制语句，由于嵌套可能存在，故将value定义为数组.其值为[(condition，index),...]
         #由于是深度优先，所以当有深层嵌套时，next必有值
-        self.next = None
-        self.layer = layer#label是第几层嵌套的label
+        self.top = -1#初始时栈指针指向栈底
+        self.layer = layer#label是第几层嵌套的label，一层对应一个labelnode
 
+        self.next = None
+
+    def show(self):
+        print("value of this LabelNode is {}".format(self.value))
+        print("layer of this LabelNode is {}".format(self.layer))
+
+    def push(self):
+        return
 #动态单链表，用于labelnode生成为每个代码块node生成label
-class Linklist(object):
+class LabelLinklist(object):
 
     def __init__(self):
         # 初始化空表
@@ -46,6 +126,13 @@ class Linklist(object):
             pre = pre.next
         return length
 
+    def show(self):
+        pre = self.head
+        while pre:
+            pre.show()
+            pre = pre.next
+        return
+
     def append(self,labelnode):
         """
             追加节点，需要新建节点（多跳转情况下需调整）\n
@@ -54,7 +141,6 @@ class Linklist(object):
             :param data:
             :return:
         """
-
         new_node = labelnode
 
         #若为空表，则新节点为表头
@@ -92,8 +178,53 @@ class Linklist(object):
         if node:
             node.data = data
         return node
+#生成label可以利用之前的codeBlockList加上栈可以解决
+class LabeStack(object):
+    def __init__(self,nodelist):
+        self.nodelist = nodelist
+        self.length = 0
+        self.top = -1
+        self.stack = []
 
-#读取数据转为字符串
+    def push(self,node):
+        if len(self.stack)<=self.length:
+            self.stack.append(node)
+            self.length+=1
+            self.top+=1
+        else:
+            self.stack[self.top+1] = node
+            self.length+=1
+            self.top+=1
+
+    def pop(self):
+        tempnode = self.stack[self.top]
+        templabel = []
+
+        for i in range(self.top+1):
+            temp = (self.stack[i].index,self.stack[i].title)
+            templabel.append(temp)
+        #todo,出栈的时候生成label
+        tempnode.label = templabel
+        self.stack[self.top] = None
+        self.top-=1
+        self.length-=1
+
+        return tempnode
+
+    def check(self):
+        #todo
+        return
+
+    def popall(self,node):
+        #todo
+        return
+
+
+
+
+
+
+#--------------1读取数据转为字符串-----------------
 def readFileToStr(filename)->str:
     try:
         f = open(filename, "r")
@@ -164,28 +295,7 @@ class brace (object):
         self.next = next
         return
 
-#用于管理一份代码的所有codeBlock（即node）
-class codeBlockList(object):
-    def __init__(self):
-        self.nodelist = []
-        self.length = 0
-    def append(self,node):
-        self.nodelist.append(node)
-    def getLength(self):
-        self.length = len(self.nodelist)
-        return self.length
-    def show(self):
-        for j in range(len(self.nodelist)):
-            print("-----------codeBlock%d---------------" % j)
-            self.nodelist[j].show()
-            print("---------------------------")
-    #各代码块顺序按在文件中的顺序排列
-    def sortByBraceOrder(self):
-        self.nodelist.sort(key=self.takeNodeBrace)
-    def takeNodeBrace(self,elem):
-        return elem.Range[0]
-
-#从代码中提取codeBlock
+#--------------2从代码中提取codeBlock----------------
 def codeBlockExtractor(content:str):
     print("-----------codeBlockExtractor---------------")
     cb = []
@@ -310,11 +420,11 @@ def codeBlockExtractor(content:str):
     #     nodeList[j].show()
     #     print("---------------------------")
 
-    nodeList.show()
+    # nodeList.show()
     print("-----------End of codeBlockExtractor---------------")
     return nodeList
 
-#从代码和codeBlockList中提取出CFG
+#-------------3从代码和codeBlockList中提取出CFG---------
 def cfgExtractor(content:str):
     print("-----------cfgExtractor---------------")
     cbList = codeBlockExtractor(content)
@@ -328,7 +438,44 @@ def cfgExtractor(content:str):
     #     print("-----------codeBlock%d---------------" % j)
     #     cbList[j].show()
     #     print("---------------------------")
+
+
+    cbList.calFindScope(content)#确定每个node的title，以便于生成label
     cbList.show()
+
+    # labelList = LabelLinklist()#创建空表
+    # print(cbList.nodelist[0].layer)
+
+    # # 若存在0层node，添加为表头
+    # if cbList.nodelist[0].layer == 0:
+    #     # print("here")
+    #     temp = LabelNode(cbList.nodelist[0].layer)
+    #     labelList.append(temp)
+    #
+    #     labelList.show()
+    # else:
+    #     print("error in cfgExtractor")
+    # index = 1
+    # pre = labelList.head
+    # # while pre and index< cbList.getLength():
+    # #     templayer = pre.layer
+    # #     #pre = pre.next
+    # #
+    # #     pass
+    # for index in range(cbList.getLength()):
+
+    ls = LabeStack(cbList)
+    for i in range(cbList.getLength()):
+        #todo
+        ls.push(cbList.nodelist[i])
+        list = ls.check()
+        ls.popall(list)
+        pass
+
+
+
+    print("-----------End of cfgExtractor-------------")
+
     return
 
 def main():
